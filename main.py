6631ec7c11ce8ea75f4f448e29487fbb7529439a
Konstantin Lopuhin <kostia.lopuhin@gmail.com>
2011-01-27 18:53:16 +0300

    coordinate grid

--- main.py
+++ main.py
@@ -20,6 +20,8 @@ class MainWindow(QtGui.QMainWindow, ui_main.Ui_MainWindow):
         self.scene_source = None
         self.eval_globals = dict(gl.__dict__)
         self.eval_globals.update(glu.__dict__)
+        self.eval_globals['grid'] = self.glwidget.draw_grid
+        del self.eval_globals['glLoadIdentity'] # do not allow
 
     def update_scene(self):
         self.scene_source = unicode(self.textEdit.toPlainText())
@@ -41,7 +43,7 @@ class MainWindow(QtGui.QMainWindow, ui_main.Ui_MainWindow):
                     lines[n] = u'<font title="%s" color="red">%s</font>' % (error, line)
                     if in_glBegin:
                         gl.glEnd()
-                        self.glwidget.clearScene()
+                        self.glwidget.clear_scene()
                     success = False
                     break
                 if eval_line == 'glEnd()':
@@ -76,16 +78,39 @@ class GLWidget(QtOpenGL.QGLWidget):
         gl.glMatrixMode(gl.GL_MODELVIEW)
         gl.glLoadIdentity()
 
-    def clearScene(self):
+    def clear_scene(self):
         gl.glClear(gl.GL_COLOR_BUFFER_BIT | gl.GL_DEPTH_BUFFER_BIT)
         gl.glLoadIdentity()
+
+    def draw_grid(self):
+        ''' Draw coordinate grid in (x, z) plane with y = 0 '''
+        grid_size = 10
+        grid_coords = range(-grid_size, grid_size + 1)
+        gl.glColor3f(0.5, 0.5, 0.5)
+        gl.glBegin(gl.GL_LINES)
+        for x in grid_coords:
+            gl.glVertex3f(x, 0,-grid_size)
+            gl.glVertex3f(x, 0, grid_size)
+        for z in grid_coords:
+            gl.glVertex3f(-grid_size, 0, z)
+            gl.glVertex3f( grid_size, 0, z)
+        # bright main coordinate lines
+        gl.glColor3f(1.0, 1.0, 1.0)
+        gl.glVertex3f(-grid_size, 0, 0)
+        gl.glVertex3f( grid_size, 0, 0)
+        gl.glVertex3f(0, 0,-grid_size)
+        gl.glVertex3f(0, 0, grid_size)
+        gl.glVertex3f(0,-grid_size, 0)
+        gl.glVertex3f(0, grid_size, 0)
+        gl.glEnd()
     
     def paintGL(self):
-        self.clearScene()
+        self.clear_scene()
         self.eval_source()
+        
 
-
-SAMPLE_SCENE = '''glTranslatef(-1.5, 0.0, -6.0)
+SAMPLE_SCENE = '''glTranslatef(-1.5, -1.0, -6.0)
+grid()
 glBegin(GL_TRIANGLES)
 glColor3f(1.0, 0.0, 0.0)
 glVertex3f(0.0, 1.0, 0.0)
@@ -95,15 +120,16 @@ glColor3f(0.0, 0.0, 1.0)
 glVertex3f(1.0, -1.0, 0.0)
 glEnd()
 
-glLoadIdentity()
-glTranslatef(1.5, 0.0, -6.0)
+glTranslatef(3.5, 0.0, 0.0)
+grid()
 glColor3f(0.5, 0.5, 1.0)
 glBegin(GL_QUADS)
 glVertex3f(-1.0, 1.0, 0.0)
 glVertex3f( 1.0, 1.0, 0.0)
 glVertex3f( 1.0,-1.0, 0.0)
 glVertex3f(-1.0,-1.0, 0.0)
-glEnd()'''
+glEnd()
+'''
 
 
 if __name__ == '__main__':
